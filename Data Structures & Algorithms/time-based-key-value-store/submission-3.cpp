e0b4d5de44483282b6fb9d0e566402763b95cc04
anu-gulhane <59767677+anu-gulhane@users.noreply.github.com>
2026-07-31 17:07:56 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.cpp
@@ -0,0 +1,32 @@
+class TimeMap {
+public:
+    map<int,map<string,string>>m;
+    set<int>s;
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int t) {
+        map<string,string>mt=m[t];
+        mt[key]=value;
+        m[t]=mt;
+        s.insert(t);
+    }
+    
+    string get(string key, int t) {
+        auto it = s.upper_bound(t);
+        if (it == s.begin())
+            return "";
+        --it;
+        while(true){
+            t=*it;
+            map<string,string>mt=m[t];
+            if(mt.count(key)==0){
+                if(it==s.begin())break;
+                it--;
+                continue;
+            }
+            return mt[key];
+        }
+        return "";
+    }
+};
