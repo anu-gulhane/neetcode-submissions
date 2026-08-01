a4902bd1a4e96a50d5630d6f43a845ec77174e50
anu-gulhane <59767677+anu-gulhane@users.noreply.github.com>
2026-08-01 19:56:36 +0530

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode *head=&dummy;
+        int carry=0;
+        while(l1 && l2){
+            int sum=l1->val+l2->val+carry;
+            if(sum>9){
+                carry=1;
+                sum=sum%10;
+            }
+            else carry =0;
+            ListNode *temp=new ListNode(sum);
+            head->next=temp;
+            head=temp;
+            l1=l1->next;
+            l2=l2->next;
+        }
+        while(l1){
+            int sum=l1->val+carry;
+            if(sum>9){
+                sum=sum%10;
+                carry=1;
+            }
+            else carry=0;
+            ListNode *temp=new ListNode(sum);
+            head->next=temp;
+            head=temp;
+            l1=l1->next;
+        }
+        while(l2){
+            int sum=l2->val+carry;
+            if(sum>9){
+                sum=sum%10;
+                carry=1;
+            }
+            else carry=0;
+            ListNode *temp=new ListNode(sum);
+            head->next=temp;
+            head=temp;
+            l2=l2->next;
+        }
+        if(carry)head->next=new ListNode(carry);
+        return dummy.next;
+    }
+};
