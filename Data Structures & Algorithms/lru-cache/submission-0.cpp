61924024537bbf8bb1173205356614fe7380004a
anu-gulhane <59767677+anu-gulhane@users.noreply.github.com>
2026-08-04 17:58:10 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.cpp
@@ -0,0 +1,31 @@
+class LRUCache {
+public:
+    int maxi;
+    unordered_map<int,pair<int,list<int>::iterator>>m;
+    list<int>l;
+    LRUCache(int capacity) {
+        maxi=capacity;
+    }
+    
+    int get(int key) {
+        if(m.count(key)==0)return -1;
+        l.erase(m[key].second);
+        l.push_back(key);
+        m[key].second=--l.end();
+        return m[key].first;
+    }
+    
+    void put(int key, int value) {
+        if(m.contains(key)){
+            //true
+            l.erase(m[key].second);
+        }
+        if(l.size()>=maxi){
+            //overload, remove lru
+            m.erase(l.front());
+            l.pop_front();
+        }
+        l.push_back(key);
+        m[key]={value,--l.end()};
+    }
+};
